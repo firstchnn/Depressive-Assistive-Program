51fe5c1db510947f39c56de9e450077af81cc63d
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2023-02-05 17:32:34 +0700

    Main chat update
    UI Update

--- Application/frontend/src/screens/MainChatScreen.js
+++ Application/frontend/src/screens/MainChatScreen.js
@@ -8,15 +8,28 @@ function MainChatScreen({navigation}) {
     const nav = useNavigation();
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{paddingBottom: '20%'}}>
       <Text>Main Chat Screen</Text>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+      <View style={{flexDirection: 'column'}}>
+      <Text>Button 1</Text>
       <Button
         title="Speaker"
         onPress={() => nav.navigate('UserChat', { role: 'Speaker' })}
       />
+      </View>
+      <View style={{width: '10%'}}></View>
+      <View style={{width: 3, backgroundColor: 'black'}} />
+      <View style={{width: '10%'}}></View>
+      <View style={{flexDirection: 'column',}}>
+      <Text>Button 2</Text>
       <Button
         title="Listener"
         onPress={() => nav.navigate('UserChat', { role: 'Listener' })}
       />
+      </View>
+    </View>
     </View>
   );
 }
