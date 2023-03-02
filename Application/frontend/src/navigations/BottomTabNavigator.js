e915843c1d61334ec1cb994599e20e327f24ce85
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2023-03-02 19:01:23 +0700

    home icon & color
    Add home icon to bottom navBar and chage the selected screen color for navBar

--- Application/frontend/src/navigations/BottomTabNavigator.js
+++ Application/frontend/src/navigations/BottomTabNavigator.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View,Image} from 'react-native';
 import React, {useEffect, useState} from 'react';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -12,14 +12,32 @@ import DoctorDetail from '../screens/DoctorDetail';
 import MainChatScreen from '../screens/MainChatScreen';
  
 const Tab = createBottomTabNavigator();
+const highlightColor = '#55dab8'; // replace with your desired color
+
 
 function BottomTabNavigator({route, navigation}) {
 
   return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+    <Tab.Navigator 
+    initialRouteName="Home" 
+    screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor:highlightColor,
+    }}
+    
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require('../asset/home-icon.png')}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ),
+        }}
         initialParams={{
           displayName: route.params.displayName,
           email: route.params.email,
@@ -27,10 +45,13 @@ function BottomTabNavigator({route, navigation}) {
           uid: route.params.uid,
         }}
       />
-      <Tab.Screen name="DoctorList" component={DoctorListScreen} />
+      <Tab.Screen 
+      name="DoctorList" 
+      component={DoctorListScreen} 
+      // options={{ tabBarBadge:0}}
+      />
       {/* <Tab.Screen name="DoctorDetail" component={DoctorDetail}/> */}
       <Tab.Screen name="MainChat" component={MainChatScreen} />
-      
       <Tab.Screen name="EvaluationScreen" component={EvaluationScreen} />
       <Tab.Screen name="SettingScreen" component={SettingScreen} />
     </Tab.Navigator>
